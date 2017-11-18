48cee48721e598be774de3e751a44ef472f7dad5
Z <mathiverse@gmail.com>
2017-11-17 19:48:54 -0800

    multiple sim reporters and ways to run sim
    You can run sim on disk or in memory depending on
    whether you know there will be an out of memory error or
    not.
    
    I also used this implementation to see if my napkin math on the
    minimum num of runs I could have in memory was correct. It was not
    correct. I still got an out of memory error. Good try though!
    
    I wonder what I did wrong...

--- src/main/scala/Simulator.scala
+++ src/main/scala/Simulator.scala
@@ -11,31 +11,51 @@ import java.io.{BufferedWriter, File, FileWriter}
 
 import scala.io.Source
 
-class Simulator(val numRuns: Int = 1, val concurrency: Int = 1, val outputFileName: String = "scores.out") {
+class Simulator(val numRuns: Int = 1, val concurrency: Int = 1, val outputFileName: String = "scores.out", val useDiskStorage: Boolean = false) {
   require(numRuns > 0, "The number of runs must be greater than zero.")
   require(concurrency == 1, "Concurrency must be 1. Greater concurrency is not supported.")
 
   private val log: Logger = LogManager.getLogger(Simulator.getClass)
-  //private var runs: List[GameState] = List.fill(numRuns)(new GameState)
+  private var runs: List[GameState] = List()
+  if (!useDiskStorage) {
+    runs = List.fill(numRuns)(new GameState)
+  }
+  private var reporter: SimulationReporter = null
+  if (useDiskStorage) {
+    reporter = new FromFileReporter(outputFileName)
+  } else {
+    reporter = new InMemoryReporter
+  }
   private var runsCompleted = false
+  final val DELIMITER = ", "
   /*** public interface ***/
-  var data: List[Tuple2[Int, Int]] = List()
   def startSimulation: Unit = {
     if (runsCompleted) {
       log.warn("The simulation is already complete. You have to clear the simulation before you can run it again.")
       return
     }
-    val file = new File(outputFileName)
-    val bw = new BufferedWriter(new FileWriter(file))
-    
-    0 until numRuns foreach(_ => {
-      val g = new GameState
-      g.finishGame
-      bw.write(s"${g.getCurrentScore}$DELIMITER${g.numOfTurns}")
-      bw.newLine
-    })
+
+    if (useDiskStorage) {
+      log.info("Running using disk storage.")
+      log.info(s"Output file: $outputFileName")
+      val file = new File(outputFileName)
+      val bw = new BufferedWriter(new FileWriter(file))
+      
+      0 until numRuns foreach(_ => {
+        val g = new GameState
+        g.finishGame
+        bw.write(s"${g.getCurrentScore}$DELIMITER${g.numOfTurns}")
+        bw.newLine
+      })
+      bw.close
+    } else {
+      log.info("Running using in memory storage.")
+      //todo: Is asInstanceOf the only way to resolve this issue?
+      // Is there a more natural way to do this?
+      runs.map(_.finishGame)
+      reporter.asInstanceOf[InMemoryReporter].setGames(runs)
+    }
     runsCompleted = true
-    bw.close
   }
   // I'm not 100% sure when this would be called, but I'm guessing it will be useful at some point.
   def stopSimulation = {
@@ -47,78 +67,161 @@ class Simulator(val numRuns: Int = 1, val concurrency: Int = 1, val outputFileNa
     //runs = List.fill(numRuns)(new GameState)
     runsCompleted = false
   }
-
-  // todo: Should this be a different class? This isn't about simulating. It's about
-  // analyzing the results of a simulation, right?
   def printScores: Unit = {
-    if (!runsCompleted) {
-      log.warn("Getting scores when runs aren't completed")
-    }
-    println(getScores)
-  }
-
-  def printAverageScore: Unit = {
-    if (!runsCompleted) {
-      log.warn("Getting scores when runs aren't completed")
-    }
-    println(getAverageScore)
+    reporter.printScores
   }
   def getScores: List[Int] = {
-    if (!runsCompleted) { 
-      log.warn("Getting scores when runs aren't completed")
-    }
-    if (data.isEmpty) {
-      data = readData(outputFileName)
-    }
-    return data.map(_._1)
+    reporter.getScores
+  }
+  def printAverageScore: Unit = {
+    reporter.printAverageScore
   }
   def getAverageScore: Int = {
-    if (!runsCompleted) {
-      log.warn("Getting scores when runs aren't completed")
+    reporter.getAverageScore
+  }
+  def printNumOfTurns: Unit = {
+    reporter.printNumOfTurns
+  }
+  def getNumOfTurns: List[Int] = {
+    reporter.getNumOfTurns
+  }
+  abstract class SimulationReporter {
+    def printScores: Unit
+    def getScores: List[Int]
+    def printAverageScore: Unit
+    def getAverageScore: Int
+    def printNumOfTurns: Unit
+    def getNumOfTurns: List[Int]
+  }
+  class InMemoryReporter extends SimulationReporter {
+    var games: List[GameState] = null
+    var scores: List[Int] = null
+    var numOfTurns: List[Int] = null
+
+    def setGames(games: List[GameState]): Unit = {
+      this.games = games
     }
-    val scores = getScores
-    scores.reduce(_ + _) / scores.size
-  }
-
-  def printNumOfTurns(): Unit = {
-    println(getNumOfTurns)
-  }
-
-  /*
-    0. See if the val with the data is already populated,
-        if no, go to 1, if yes, return the val.
-    1. Open file with scores.
-    2. Iterate over them.
-      2.1 Parse the data point.
-      2.2 Add the second value in the list to the list
-            of numOfTurns.
-    3. Close the file.
-    4. Save the list to a val.
-  */
-  def getNumOfTurns(): List[Int] = {
-    //runs.map(_.numOfTurns)
-    if (data.isEmpty) {
-      data = readData(outputFileName)
+    override def printScores: Unit = {
+      assert(games != null)
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      println(getScores)
     }
-    data.map(_._2)
+
+   override def printAverageScore: Unit = {
+      assert(games != null)
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      println(getAverageScore)
+    }
+    override def getScores: List[Int] = {
+      assert(games != null)
+      if (!runsCompleted) { 
+        log.warn("Getting scores when runs aren't completed")
+      }
+      if (scores == null) {
+        scores = runs.map(_.getCurrentScore)
+      }
+      scores
+    }
+    override def getAverageScore: Int = {
+      assert(games != null)
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      if (scores == null) {
+        getScores
+      }
+      scores.reduce(_ + _) / scores.size
+    }
+
+    override def printNumOfTurns: Unit = {
+      assert(games != null)
+      println(getNumOfTurns)
+    }
+
+    override def getNumOfTurns: List[Int] = {
+      assert(games != null)
+      if (numOfTurns == null) {
+        numOfTurns = runs.map(_.numOfTurns)
+      }
+      numOfTurns
+    }
   }
 
-  final val DELIMITER = ", "
-  def readData(file: String): List[Tuple2[Int, Int]] = {
-    val bufferedSource = Source.fromFile(file)
+  class FromFileReporter(val resultFile: String) extends SimulationReporter {
     var data: List[Tuple2[Int, Int]] = List()
-    for (line <- bufferedSource.getLines) {
-      val datums = line.split(DELIMITER).map(_.toInt)
-      data = (datums(0), datums(1)) :: data
+    override def printScores: Unit = {
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      println(getScores)
     }
-    bufferedSource.close
-    data
+
+    override def printAverageScore: Unit = {
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      println(getAverageScore)
+    }
+    override def getScores: List[Int] = {
+      if (!runsCompleted) { 
+        log.warn("Getting scores when runs aren't completed")
+      }
+      if (data.isEmpty) {
+        data = readData(outputFileName)
+      }
+      return data.map(_._1)
+    }
+    override def getAverageScore: Int = {
+      if (!runsCompleted) {
+        log.warn("Getting scores when runs aren't completed")
+      }
+      val scores = getScores
+      scores.reduce(_ + _) / scores.size
+    }
+
+    override def printNumOfTurns(): Unit = {
+      println(getNumOfTurns)
+    }
+
+    /*
+      0. See if the val with the data is already populated,
+          if no, go to 1, if yes, return the val.
+      1. Open file with scores.
+      2. Iterate over them.
+        2.1 Parse the data point.
+        2.2 Add the second value in the list to the list
+              of numOfTurns.
+      3. Close the file.
+      4. Save the list to a val.
+    */
+    override def getNumOfTurns(): List[Int] = {
+      //runs.map(_.numOfTurns)
+      if (data.isEmpty) {
+        data = readData(outputFileName)
+      }
+      data.map(_._2)
+    }
+
+    private def readData(file: String): List[Tuple2[Int, Int]] = {
+      val bufferedSource = Source.fromFile(file)
+      var data: List[Tuple2[Int, Int]] = List()
+      for (line <- bufferedSource.getLines) {
+        val datums = line.split(DELIMITER).map(_.toInt)
+        data = (datums(0), datums(1)) :: data
+      }
+      bufferedSource.close
+      data
+    }
   }
 }
 
 object Simulator {
   val log: Logger = LogManager.getLogger(this.getClass)
-  val usage = "Usage: -n [numRuns] -f [outputFile]"
+  val usage = "Usage: -n [numRuns] -f [outputFile] -d [true/false]"
   def main(args: Array[String]) {
     type OptionMap = Map[Symbol, Any]
     // todo: have some way to print the usage string
@@ -131,6 +234,8 @@ object Simulator {
                                nextOption(map ++ Map('numRuns -> value.toInt), tail)
         case "-f" :: value :: tail =>
                                nextOption(map ++ Map('outputFile -> value.toString), tail)
+        case "-d" :: value :: tail =>
+                               nextOption(map ++ Map('diskStorage -> value.toBoolean), tail)
         case unknown :: tail => {
             log.info(s"Unknown option: $unknown")
             nextOption(map, tail)
@@ -143,7 +248,7 @@ object Simulator {
     //1727115
     //1000000000
     //val simulator = new Simulator(numRuns = 1000000000)
-    simulator = new Simulator(numRuns = options.getOrElse('numRuns, 100).asInstanceOf[Int], outputFileName = options.getOrElse('outputFile, "scores.out").asInstanceOf[String])
+    simulator = new Simulator(numRuns = options.getOrElse('numRuns, 100).asInstanceOf[Int], outputFileName = options.getOrElse('outputFile, "scores.out").asInstanceOf[String], useDiskStorage = options.getOrElse('diskStorage, false).asInstanceOf[Boolean])
     simulator.startSimulation
     simulator.printScores
     simulator.printNumOfTurns
